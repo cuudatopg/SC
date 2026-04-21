3e833284142d585db747cddfdb5e786c6e4396c7
cuudatopg <cuuatschool@gmail.com>
2026-04-21 14:52:11 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout";
-{/*import ChatPage from "./pages/chat/ChatPage";*/}
 import AlbumPage from "./pages/album/AlbumPage";
 import AdminPage from "./pages/admin/AdminPage";
 import SearchPage from "./pages/search/searchPage";
@@ -24,7 +23,6 @@ function App() {
 
 				<Route element={<MainLayout />}>
 					<Route path='/' element={<HomePage />} />
-					{/*<Route path='/chat' element={<ChatPage />} /> */}
 					<Route path='/albums/:albumId' element={<AlbumPage />} />
 					<Route path='*' element={<NotFoundPage />} />
 					<Route path='/search' element={<SearchPage />} />
