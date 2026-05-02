28aadcbdc64a7c7216d5cf01dfc9b011d98b1c03
cuudatopg <cuuatschool@gmail.com>
2026-05-02 08:51:06 +0700

    update user info change

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,24 +1,37 @@
 import { User } from "../models/user.model.js";
 
 export const authCallback = async (req, res, next) => {
-	try {
-		const { id, firstName, lastName, imageUrl } = req.body;
+    try {
+        const { id, firstName, lastName, imageUrl } = req.body;
+        const fullName = `${firstName || ""} ${lastName || ""}`.trim();
 
-		// check if user already exists
-		const user = await User.findOne({ clerkId: id });
+        let user = await User.findOne({ clerkId: id });
 
-		if (!user) {
-			// signup
-			await User.create({
-				clerkId: id,
-				fullName: `${firstName || ""} ${lastName || ""}`.trim(),
-				imageUrl,
-			});
-		}
+        if (!user) {
+            await User.create({
+                clerkId: id,
+                fullName,
+                imageUrl,
+            });
+        } else {
+            const isNameChanged = user.fullName !== fullName;
+            const isImageChanged = user.imageUrl !== imageUrl;
 
-		res.status(200).json({ success: true });
-	} catch (error) {
-		console.log("Error in auth callback", error);
-		next(error);
-	}
+            if (isNameChanged || isImageChanged) {
+                await User.findOneAndUpdate(
+                    { clerkId: id },
+                    { 
+                        fullName: fullName,
+                        imageUrl: imageUrl 
+                    },
+                    { new: true } 
+                );
+            }
+        }
+
+        res.status(200).json({ success: true });
+    } catch (error) {
+        console.log("Error in auth callback", error);
+        next(error);
+    }
 };
